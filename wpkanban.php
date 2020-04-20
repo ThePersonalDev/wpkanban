b810111c58a278326899d008d32ab992e477cc29
Oz Ramos <thepersonaldev@gmail.com>
2020-04-20 14:26:07 -0700

    #1 Adds basic plugin header

---
+++ wpkanban.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin name: WP Kanban
+ * Plugin URI: https://github.com/ThePersonalDev/wpkanban
+ * Description: A kanban dashboard widget to help you project manage your WordPress site!
+ * Author: Oz Ramos
+ * Author URI: https://thepersonaldev.com
+ * License: Apache 2.0
+ * License URI: http://www.apache.org/licenses/LICENSE-2.0
+ */
