9c26615dda2f431988cb395c291e8c57bd95c819
Oz Ramos <thepersonaldev@gmail.com>
2020-04-20 19:00:39 -0700

    #4 Apply drag to cards within same column

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
   div
     Container.wpkanban-container(orientation='horizontal' @drop='onColumnDrop')
-      Draggable.wpkanban-list-column(v-for='(list, key) in lists' :key='key')
+      Draggable.wpkanban-list-column(v-for='list in board' :key='list.id')
         h3 {{list.title}}
-      
+        Container(group-name='col' @drop='(e) => onCardDrop(list.id, e)')
+          Draggable.wpkanban-card-mini(v-for='(card, key) in list.cards' :key='key')
+            div {{card.title}}
       .clear
 </template>
 
@@ -16,25 +18,45 @@ export default {
   components: {Container, Draggable},
   
   data: () => ({
-    lists: [
+    board: [
       {
-        title: 'Todo'
+        title: 'Todo',
+        id: 0,
+        cards: [{title: 'Test A'}, {title: 'Test B'}, {title: 'Test C'}]
       },
       {
-        title: 'Doing'
+        title: 'Doing',
+        id: 1,
+        cards: [{title: 'Test D'}, {title: 'Test E'}, {title: 'Test F'}]
       },
       {
-        title: 'Done'
+        title: 'Done',
+        id: 2,
+        cards: [{title: 'Test G'}, {title: 'Test H'}, {title: 'Test I'}]
       }
     ]
   }),
 
   methods: {
     onColumnDrop ($event) {
-      this.lists = this.applyDrop(this.lists, $event)
+      this.board = this.applyDrag(this.board, $event)
     },
 
-    applyDrop (set, dragResult) {
+    onCardDrop (listId, dropResult) {
+      if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+        const board = this.board.slice()
+        const list = board.filter(list => list.id === listId)[0]
+        const listIdx = board.indexOf(list)
+        const newList = Object.assign({}, list)
+
+        newList.cards = this.applyDrag(newList.cards, dropResult)
+        board.splice(listIdx, 1, newList)
+
+        this.board = board
+      }
+    },
+
+    applyDrag (set, dragResult) {
       const {removedIndex, addedIndex, payload} = dragResult
       if (removedIndex === null && addedIndex === null) return set
 
@@ -83,5 +105,10 @@ $bg-1: #fff;
 }
 .smooth-dnd-container.horizontal > .smooth-dnd-draggable-wrapper {
   display: inline-block;
+}
+
+.wpkanban-card-mini {
+  padding: 10px;
+  border: 1px solid $color-border;
 }
 </style>
