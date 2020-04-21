50c5ab42342e5b75c41516e8919bb0cb142a0a63
Oz Ramos <thepersonaldev@gmail.com>
2020-04-20 17:50:04 -0700

    #3 Makes columns draggable (but does not persist state)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template lang="pug">
   div
-    div.list-column(v-for='(list, key) in lists' :key='key')
-      h3 {{list.title}}
-    .clear
+    Container.wpkanban-container(orientation='horizontal')
+      Draggable.wpkanban-list-column(v-for='(list, key) in lists' :key='key')
+        h3 {{list.title}}
+      
+      .clear
 </template>
 
 <script>
+import {Container, Draggable} from 'vue-smooth-dnd'
+
 export default {
   name: 'Home',
+
+  components: {Container, Draggable},
   
   data: () => ({
     lists: [
@@ -27,17 +33,31 @@ export default {
 
 <style lang="scss">
 $color-border: #ccd0d4;
+$bg-1: #fff;
 
-.list-column {
+.wpkanban-container {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.wpkanban-list-column {
   width: 300px;
   border: 1px solid $color-border;
   padding: 10px;
-  float: left;
   margin-right: 20px;
+  background: $bg-1;
 
   h3 {
     font-size: 1rem;
     margin: 0;
   }
+}
+
+.smooth-dnd-container.horizontal {
+  display: block;
+  padding-bottom: 20px;
+}
+.smooth-dnd-container.horizontal > .smooth-dnd-draggable-wrapper {
+  display: inline-block;
 }
 </style>
