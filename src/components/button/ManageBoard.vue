a5bd6b21103fa40621765b29f303f37f6bac4d92
Oz Ramos <thepersonaldev@gmail.com>
2020-06-03 20:47:05 -0700

    #27 Starts manage board button, fixes bug where modals weren't closing

---
+++ src/components/button/ManageBoard.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  a.button.tpd-m-l-10.thickbox(:href='thickboxURL' title='Manage Board' @click='showModal')
+    | Manage Board
+    Modal#wpkanban-manage-board-modal(v-if='isModalVisible' :thickbox='thickbox' v-on:close='isModalVisible = false')
+      div.input-text-wrap.tpd-m-t-10
+        label(for='wpkanban-manage-board-title')
+          strong Board Name:
+        div.tpd-m-t-10
+          input#wpkanban-manage-board-title(ref='title' type='text' autocomplete='off' v-model='newBoardTitle' style='width: 100%' v-on:keyup.enter='createBoard')
+      p
+        button.button.button-primary(:disabled='!newBoardTitle' @click='updateBoard') Update Board
+        button.button.button-error.tpd-float-r(@click='deleteBoard') Delete Board
+</template>
+
+<script>
+import Modal from '../Modal.vue'
+import {mapState} from 'vuex'
+
+export default {
+  components: {Modal},
+
+  computed: {
+    ...mapState(['board']),
+    
+    thickboxURL: function () {
+      return `#TB_inline?width=${this.thickbox.width}&height=${this.thickbox.height}&inlineId=wpkanban-manage-board-modal`
+    }
+  },
+
+  watch: {
+    'board.currentBoard.title' (title) {
+      this.newBoardTitle = title
+    }
+  },
+
+  data: () => ({
+    isModalVisible: false,
+    
+    shouldCreateDefaultColumns: true,
+    newBoardTitle: '',
+
+    thickbox: {
+      width: 350,
+      height: 140
+    }
+  }),
+
+  mounted () {
+    this.newBoardTitle = this.board.currentBoard.title
+  },
+
+  methods: {
+    showModal () {
+      this.shouldCreateDefaultColumns = true
+      this.isModalVisible = true
+    },
+    
+    /**
+     * Create a new board and switch to it
+     */
+    updateBoard () {
+      console.log('updateBoard')
+      this.isModalVisible = false
+    },
+
+    deleteBoard () {
+      console.log('delete')
+      this.isModalVisible = false
+    }
+  }
+}
+</script>
